12aa16b3cd4d89842092c6dc410588b28e26ddcf
Udan Jayanith <j.udanjayanith@gmail.com>
2025-07-28 00:29:59 +0530

    Add new favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,7 @@ export default defineUserConfig({
 	description: 'This is from config.js',
 
 	theme: defaultTheme({
-		logo: 'https://raw.githubusercontent.com/udan-jayanith/GoHTML-Documentation-Site/refs/heads/main/assets/media/Transparent%20Black%20version.png', //navigation bar ico
+		logo: 'https://raw.githubusercontent.com/udan-jayanith/GoHTML-Documentation-Site/refs/heads/main/assets/media/Favicon%20black%20transparent%20version.png.png', //navigation bar ico
 		navbar: ['/', '/get-started'],
 		colorMode: 'light', // set default to light
 		colorModeSwitch: false, // hide the toggle
